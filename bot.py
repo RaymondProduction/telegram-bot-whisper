422373d63a1712ae4b1ab51f7d547e395f7cc87f
Raymond Perminov <raymond@brizoit.com>
2025-03-20 19:02:27 +0200

    Added send_long_message

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import whisper
+import subprocess
 import os
 import logging
 import requests
@@ -13,11 +14,11 @@ print(f"Available models: {available_models}")
 AUDIO_DIR = "audio"
 MODEL = "small"
 
-THREADS = 4  # Number of threads for Whisper.cpp
+THREADS = 8  # Number of threads for Whisper.cpp
 torch.set_num_threads(THREADS)
 
 # Load model once at startup
-model = whisper.load_model(MODEL)
+model = whisper.load_model(MODEL, device = "cpu")
 
 # Configure logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
@@ -62,12 +63,14 @@ def save_chat_id(chat_id):
 
 def transcribe_audio(file_path):
     # Convert MP3 to WAV
-    #wav_path = audio_path.replace(".mp3", ".wav")
-    #subprocess.run(["ffmpeg", "-i", audio_path, "-ar", "16000", "-ac", "1", "-c:a", "pcm_s16le", wav_path], check=True)
+    wav_path = file_path.replace(".mp3", ".wav")
+
+    print(f"wav_path = {wav_path}")
+    # subprocess.run(["ffmpeg", "-i", file_path, "-ar", "16000", "-ac", "1", "-c:a", "pcm_s16le", wav_path], check=True)
 
     # result = subprocess.run(
     #     ["./whisper.cpp/build/bin/whisper-cli",
-    #       "--model", WHISPER_MODEL, 
+    #       "--model", "/whisper.cpp/models/ggml-small.bin", 
     #       "--file", wav_path,
     #       "--no-timestamps", "true",
     #       "--language", "uk",
@@ -113,20 +116,25 @@ async def handle_audio(update: Update, context: CallbackContext) -> None:
     await update.message.reply_text("Обробляю... Це може зайняти деякий час.")
     
     # Try forwarding to the secondary bot first
-    transcribed_text = forward_to_secondary_bot(file_path, chat_id)
-    if transcribed_text:
-        await update.message.reply_text(f"Розпізнано вторинним ботом:\n{transcribed_text}")
-        return
+    # transcribed_text = forward_to_secondary_bot(file_path, chat_id)
+    # if transcribed_text:
+    #     await update.message.reply_text(f"Розпізнано вторинним ботом:\n{transcribed_text}")
+    #     return
     
     # If the secondary bot fails, process locally
     try:
         text = transcribe_audio(file_path)
-        await update.message.reply_text(f"Розпізнаний текст:\n{text}")
+        print(f"Розпізнаний текст: {text}")
+        #await update.message.reply_text(f"Розпізнаний текст:\n{text}")
+        await send_long_message(chat_id, f"Розпізнаний текст:\n{text}", context)
     except Exception as e:
         await update.message.reply_text("Сталася помилка під час розпізнавання аудіо.")
         logger.error(f"Помилка: {e}")
 
-
+async def send_long_message(chat_id, text, context):
+    max_length = 4096
+    for i in range(0, len(text), max_length):
+        await context.bot.send_message(chat_id=chat_id, text=text[i:i + max_length])
 
 # Function for /start command
 async def start(update: Update, context: CallbackContext) -> None:
@@ -137,7 +145,7 @@ def main():
     app = Application.builder().token(TOKEN).build()
     
     app.add_handler(CommandHandler("start", start))
-    app.add_handler(MessageHandler(filters.VOICE | filters.AUDIO, handle_audio))
+    app.add_handler(MessageHandler(filters.VOICE | filters.AUDIO | filters.ATTACHMENT, handle_audio))
     
     print("Bot is running!")
     app.run_polling()
